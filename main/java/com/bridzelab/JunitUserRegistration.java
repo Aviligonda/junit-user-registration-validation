b1f4f93530b8ef577d6b8800310ebb7fe38396e3
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-30 22:52:10 +0530

    Added : User name,phone,email,password using regex

--- main/java/com/bridzelab/JunitUserRegistration.java
+++ main/java/com/bridzelab/JunitUserRegistration.java
@@ -1,8 +1,26 @@
 package com.bridzelab;
 
-public class JunitUserRegistration {
-    public static void main(String[] args) {
+import java.util.regex.Pattern;
 
-        System.out.println("Welcome Junit User registration program");
+public class JunitUserRegistration {
+    public boolean firstNameValidation(String firstName) {
+        return Pattern.matches("[A-Z][a-z]{3,}", firstName);
     }
+
+    public boolean lastNameValidation(String lastName) {
+        return Pattern.matches("[A-Z][a-z]{3,}", lastName);
+    }
+
+    public boolean emailValidation(String email) {
+        return Pattern.matches("[a-z\\d_-]{5,}@[a-z]{3,}.[a-z]{3,}.[a-z]{2,}", email);
+    }
+
+    public boolean phoneNumberValidation(String phoneNumber) {
+        return Pattern.matches("[+]91 [6789]\\d{9}", phoneNumber);
+    }
+
+    public boolean passwordValidation(String password) {
+        return Pattern.matches("(?=.*?[A-Z])(?=.*?\\d)(?=.*?[!@#$%^&*_()+-])[A-Za-z\\d!@#$%^&()*+_-]{8,}", password);
+    }
+
 }
--- test/java/com/bridzelab/JunitUserRegistrationTest.java
+++ test/java/com/bridzelab/JunitUserRegistrationTest.java
@@ -1,5 +1,69 @@
 package com.bridzelab;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JunitUserRegistrationTest {
+    private static final JunitUserRegistration userRegistration = new JunitUserRegistration();
+
+    @Test
+    public void positiveFirstNameValidationTest() {
+        boolean actualOutPut = userRegistration.firstNameValidation("Aviligonda");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void negativeFirstNameValidationTest() {
+        boolean actualOutPut = userRegistration.firstNameValidation("aviligonda");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void positiveLastNameValidationTest() {
+        boolean actualOutPut = userRegistration.lastNameValidation("Srinivas");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void negativeLastNameValidationTest() {
+        boolean actualOutPut = userRegistration.lastNameValidation("srinivas");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void positiveEmailValidationTest() {
+        boolean actualOutPut = userRegistration.emailValidation("aviligonda_sreenivasulu-123@gamil.com.in");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void negativeEmailValidationTest() {
+        boolean actualOutPut = userRegistration.emailValidation("srinivas@com.in");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void positivePhoneNumberValidationTest() {
+        boolean actualOutPut = userRegistration.phoneNumberValidation("+91 8919584625");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void negativePhoneNumberValidationTest() {
+        boolean actualOutPut = userRegistration.phoneNumberValidation("+918919584625");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void positivePasswordValidationTest() {
+        boolean actualOutPut = userRegistration.passwordValidation("Srinivas6#");
+        Assert.assertTrue(actualOutPut);
+    }
+
+    @Test
+    public void negativePasswordValidationTest() {
+        boolean actualOutPut = userRegistration.passwordValidation("srinivas6*");
+        Assert.assertTrue(actualOutPut);
+    }
 
 }
