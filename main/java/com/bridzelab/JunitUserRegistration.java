b1711d7a17849b194125b5d7387dfce129b50e9c
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-30 22:54:46 +0530

    Added : Check valid and invalid emails

--- main/java/com/bridzelab/JunitUserRegistration.java
+++ main/java/com/bridzelab/JunitUserRegistration.java
@@ -23,4 +23,11 @@ public class JunitUserRegistration {
         return Pattern.matches("(?=.*?[A-Z])(?=.*?\\d)(?=.*?[!@#$%^&*_()+-])[A-Za-z\\d!@#$%^&()*+_-]{8,}", password);
     }
 
+    public boolean validEmailsValidation(String email) {
+        return Pattern.matches("[-a-z.+_\\d]{3,}@[a-z\\d]*.[a-z]{2,}", email);
+    }
+
+    public boolean inValidEmailValidation(String email) {
+        return Pattern.matches("[a-z\\d]{3,}@[a-z]{4,}.[a-z]{3,}.[a-z]{2,}", email);
+    }
 }
--- test/java/com/bridzelab/JunitUserRegistrationTest.java
+++ test/java/com/bridzelab/JunitUserRegistrationTest.java
@@ -66,4 +66,54 @@ public class JunitUserRegistrationTest {
         Assert.assertTrue(actualOutPut);
     }
 
+    @Test
+    public void validEmailsValidationTest() {
+        boolean actualOutPut = userRegistration.validEmailsValidation("abc@yahoo.com");
+        Assert.assertTrue(actualOutPut);
+        boolean actualOutPut1 = userRegistration.validEmailsValidation("abc-100@yahoo.com");
+        Assert.assertTrue(actualOutPut1);
+        boolean actualOutPut2 = userRegistration.validEmailsValidation("abc.100@yahoo.com");
+        Assert.assertTrue(actualOutPut2);
+        boolean actualOutPut3 = userRegistration.validEmailsValidation("abc111@abc.com");
+        Assert.assertTrue(actualOutPut3);
+        boolean actualOutPut4 = userRegistration.validEmailsValidation("abc-100@abc.net");
+        Assert.assertTrue(actualOutPut4);
+        boolean actualOutPut5 = userRegistration.validEmailsValidation("abc@1.com");
+        Assert.assertTrue(actualOutPut5);
+        boolean actualOutPut6 = userRegistration.validEmailsValidation("abc+100@gmail.com");
+        Assert.assertTrue(actualOutPut6);
+    }
+
+    @Test
+    public void inValidEmailsValidationTest() {
+        boolean actualOutPut = userRegistration.inValidEmailValidation("abc");
+        Assert.assertFalse(actualOutPut);
+        boolean actualOutPut1 = userRegistration.inValidEmailValidation("abc@.com.my");
+        Assert.assertFalse(actualOutPut1);
+        boolean actualOutPut2 = userRegistration.inValidEmailValidation("abc123@gmail.a");
+        Assert.assertFalse(actualOutPut2);
+        boolean actualOutPut3 = userRegistration.inValidEmailValidation("abc123@.com");
+        Assert.assertFalse(actualOutPut3);
+        boolean actualOutPut4 = userRegistration.inValidEmailValidation("abc123@.com.com");
+        Assert.assertFalse(actualOutPut4);
+        boolean actualOutPut5 = userRegistration.inValidEmailValidation(".abc@abc.com");
+        Assert.assertFalse(actualOutPut5);
+        boolean actualOutPut6 = userRegistration.inValidEmailValidation("abc()*@gmail.com");
+        Assert.assertFalse(actualOutPut6);
+        boolean actualOutPut7 = userRegistration.inValidEmailValidation("abc()*@gmail.com");
+        Assert.assertFalse(actualOutPut7);
+        boolean actualOutPut8 = userRegistration.inValidEmailValidation("abc@%*.com");
+        Assert.assertFalse(actualOutPut8);
+        boolean actualOutPut9 = userRegistration.inValidEmailValidation("abc..2002@gmail.com");
+        Assert.assertFalse(actualOutPut9);
+        boolean actualOutPut10 = userRegistration.inValidEmailValidation("abc.@gmail.com");
+        Assert.assertFalse(actualOutPut10);
+        boolean actualOutPut11 = userRegistration.inValidEmailValidation("abc@abc@gmail.com");
+        Assert.assertFalse(actualOutPut11);
+        boolean actualOutPut12 = userRegistration.inValidEmailValidation("abc@gmail.com.1a ");
+        Assert.assertFalse(actualOutPut12);
+        boolean actualOutPut13 = userRegistration.inValidEmailValidation("abc@gmail.com.aa.au");
+        Assert.assertFalse(actualOutPut13);
+    }
+
 }
